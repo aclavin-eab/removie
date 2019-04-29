1047d4be4916c696d49e0f3011b0cfb50802ea87
Andrew Clavin <andrewclavin8@gmail.com>
2019-04-28 22:54:15 -0400

    feat(schema): graphql schema for reviews

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 
-const MongoClient = require('mongodb').MongoClient;
-
+const MongoClient = require('mongodb').MongoClient
+const uri = "mongodb://localhost:27017"
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/removie", (err, db) => {
+// (async function() {
+//   try {
+//
+//     const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+//     // ... anything
+//     console.log(client)
+//     client.close();
+//   } catch(e) {
+//     console.error(e)
+//   }
+// })()
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  // Client returned
+  var db = client.db('removie');
+  let reviwes
+  db.collection('reviews').find({}, {'text': 1, _id: 0}).toArray((err, docs) => {
+    console.log("Found the following records");
+    console.log(docs)
+    reviews = docs.map(doc => doc.text)
+    console.log(reviews)
+    //callback(docs);
+  });
+  const express = require('express')
+  const graphqlHTTP = require('express-graphql')
+  const { buildSchema } = require('graphql')
+  const schema = buildSchema(`
+      type Query {
+          hello: String
+          review: [String]
+      }
+  `)
 
-     if(err) throw err;
-     console.log('db running', db)
-     //Write databse Insert/Update/Query code here..
+  const root = {
+    hello: () => {
+        return 'Hello world!'
+    },
+    review: () => {
+        return reviews
+    }
+  }
 
+  const app = express()
+  app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  }))
+  app.listen(4000)
+  console.log('Running a GraphQL API server at localhost:4000/graphql')
 });
